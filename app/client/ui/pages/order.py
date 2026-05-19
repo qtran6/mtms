21a9227319047d9391046a8b14c89a1919af7c24
qtran6 <qtran6@ewu.edu>
2026-05-19 16:48:05 +0700

    Refactor OrderPage: improve layout structure and separate customer name input into its own method

--- app/client/ui/pages/order.py
+++ app/client/ui/pages/order.py
@@ -1,22 +1,45 @@
-from PySide6.QtWidgets import QLabel, QGridLayout, QTableWidget, QWidget
+from PySide6.QtWidgets import QHBoxLayout, QLabel, QLineEdit, QGridLayout, QTableWidget, QWidget
 
 class OrderPage(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+
         self._grid = QGridLayout(self)
         self._grid.setContentsMargins(10, 10, 10, 10)
-        self._grid.setSpacing(10)
-        self.createTable()
-        self._grid.addWidget(self._customer_name_input, 0, 0)
+        # self._grid.setColumnStretch()
 
-    def createTable(self):
-        self._customer_name_input = QLabel("Tên khách hàng:", self)
+        self._customer_name_layout = self._input_customer_name()
+        self._table = self._createTable()
 
-        self._table = QTableWidget(self)
-        self._table.setColumnCount(5)
-        self._table.setHorizontalHeaderLabels(["TT", "Tên HH", "Số lượng", "Đơn giá", "Thành tiền"])
-        self._table.setRowCount(10)
-        self._grid.addWidget(self._table, 1, 0, 1, 2)
+        self._grid.addLayout(self._customer_name_layout, 0, 0)
+        self._grid.addWidget(self._table, 1, 0)
+
+
+    def _input_customer_name(self) -> QHBoxLayout:
+        customer_name_layout = QHBoxLayout()
+
+        text = QLabel("Tên khách hàng:")
+        text_box = QLineEdit()
+        text_box.setPlaceholderText("Nhập tên khách hàng...")
+
+        customer_name_layout.addWidget(text)
+        customer_name_layout.addWidget(text_box)
+        
+        return customer_name_layout
+
+    def _createTable(self) -> QTableWidget:
+        table = QTableWidget(self)
+
+        table.setColumnCount(5)
+        table.setHorizontalHeaderLabels(["TT", "Tên HH", "Số lượng", "Đơn giá", "Thành tiền"])
+        table.setRowCount(10)
+
+        return table
+
+
+    def _addItemUI(self):
+        ...
+
 
     def apply_theme(self, t: dict): 
         self.setStyleSheet(f"background: {t['background']};")
